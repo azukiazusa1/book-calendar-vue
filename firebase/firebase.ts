8ca7023514fe7d001495a749630fcd8db008aa6d
azukiazusa1 <cycleippei@gmail.com>
2020-12-29 22:33:26 +0900

    firebase

---
+++ firebase/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      apiKey: process.env.VUE_APP_APIKEY,
+      authDomain: process.env.VUE_APP_AUTHDOMAIN,
+      databaseURL: process.env.VUE_APP_DATABASEURL,
+      projectId: process.env.VUE_APP_PROJECTID,
+      storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+      messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+      appId: process.env.VUE_APP_APPID,
+    }
+  )
+}
+
+export default firebase
