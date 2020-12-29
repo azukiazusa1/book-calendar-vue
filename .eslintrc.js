c64572ba503b50d596e6a0c8b510ca873fc6cb81
azukiazusa1 <cycleippei@gmail.com>
2020-12-29 23:36:24 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'standard'
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -14,10 +15,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'semi': ['error', 'never'],
-    'quotes': ["error", "single"],
     'vue/no-deprecated-slot-attribute': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   },
   overrides: [
     {
