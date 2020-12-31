836b54400f00f78463321c342a6d63e1769c2737
azukiazusa1 <cycleippei@gmail.com>
2020-12-31 22:29:44 +0900

    index.vue

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <login-buttons />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LoginButtons from '@/components//organism/LoginButtons.vue'
+
+export default defineComponent({
+  components: {
+    LoginButtons
+  }
+
+})
+</script>
