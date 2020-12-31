502ac3710321460fc715a441149d094255322e30
azukiazusa1 <cycleippei@gmail.com>
2020-12-31 22:36:20 +0900

    login button

---
+++ src/components/molecules/LoginButton.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <ion-button :color="provider">
+    {{ provider }}でログイン
+      <ion-icon slot="start" color="light" :icon="icon" />
+  </ion-button>
+</template>
+
+<script>
+import { IonButton, IonIcon } from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { IonButton, IonIcon },
+  props: {
+    provider: {
+      type: String,
+      requreid: true
+    },
+    icon: {
+      type: String,
+      requreid: false,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+.ion-color-google {
+  --ion-color-base: var(--ion-color-google) !important;
+}
+</style>
---
+++ src/components/organism/LoginButtons.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <login-button provider="google" :icon="logoGoogle" />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import LoginButton from '@/components/molecules/LoginButton.vue'
+import { logoGoogle } from 'ionicons/icons'
+
+export default defineComponent({
+  components: {
+    LoginButton
+  },
+  setup () {
+    return {
+      logoGoogle
+    }
+  }
+})
+</script>
+
+<style scoped>
+.ion-color-google {
+  --ion-color-base: var(--ion-color-google) !important;
+}
+</style>
