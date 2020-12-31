54cd6282a15554a949324818fb9d1787951ca2ba
azukiazusa1 <cycleippei@gmail.com>
2020-12-31 22:27:07 +0900

    the header

---
+++ src/components/organism/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>Tab 1</ion-title>
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script>
+import {
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    IonHeader,
+    IonTitle,
+    IonToolbar
+  }
+})
+</script>
