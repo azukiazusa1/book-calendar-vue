63d56d6e31e970e693afd5370d5b071e3339ae24
azukiazusa1 <cycleippei@gmail.com>
2021-01-01 22:03:13 +0900

    tab pages

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,5 +1,4 @@
 <template>
-  <ion-page>
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
@@ -18,16 +17,15 @@
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
-  </ion-page>
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue'
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon } from '@ionic/vue'
 import { ellipse, square, triangle } from 'ionicons/icons'
 
 export default {
   name: 'Tabs',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon },
   setup () {
     return {
       ellipse,
