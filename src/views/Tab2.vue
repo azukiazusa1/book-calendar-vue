0524e2ca4ad22f3fad0a10e4549cdec3c4aa0261
azukiazusa1 <cycleippei@gmail.com>
2021-01-02 23:46:15 +0900

    title

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -6,11 +6,17 @@
 
 <script lang="ts">
 import { IonPage } from '@ionic/vue'
+import { useTitle } from 'vue-composable'
+import { APP_TITLE } from '@/constant'
 
 export default {
   name: 'Tab2',
   components: {
     IonPage
+  },
+  setup () {
+    const title = useTitle()
+    title.value = `本を探す | ${APP_TITLE}`
   }
 }
 </script>
