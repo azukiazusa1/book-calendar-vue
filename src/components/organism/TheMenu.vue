64f2198785a569bf6f22f064452e121bc9122ecc
azukiazusa1 <cycleippei@gmail.com>
2021-01-02 17:40:47 +0900

    logout

--- src/components/organism/TheMenu.vue
+++ src/components/organism/TheMenu.vue
@@ -6,19 +6,19 @@
     :disabled="!user"
     v-if="user"
   >
-    <ion-header>
-      <ion-item>
-        <user-avatar slot="start" :user="user" />
-        <ion-label>{{ user.displayName }}</ion-label>
-      </ion-item>
-    </ion-header>
-    <ion-content id="main" class="ion-padding">
+    <ion-content id="main">
       <ion-list>
-        <ion-item>ログアウト</ion-item>
-        <ion-item>Menu Item</ion-item>
-        <ion-item>Menu Item</ion-item>
-        <ion-item>Menu Item</ion-item>
-        <ion-item>Menu Item</ion-item>
+        <ion-list-header class="ion-padding">
+          <ion-item>
+            <user-avatar slot="start" :user="user" />
+            <ion-label>{{ user.displayName }}</ion-label>
+          </ion-item>
+        </ion-list-header>
+        <ion-item @click="logout">
+          <ion-button fill="clear" color="dark" size="default">
+            ログアウト
+          </ion-button>
+        </ion-item>
       </ion-list>
     </ion-content>
   </ion-menu>
@@ -26,25 +26,38 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { IonMenu, IonHeader, IonContent, IonList, IonItem, IonLabel } from '@ionic/vue'
+import { IonMenu, IonListHeader, IonContent, IonList, IonItem, IonLabel, IonButton } from '@ionic/vue'
 import UserAvatar from '@/components/atoms/UserAvatar.vue'
 import { useUserStore } from '@/store/user'
+import RegistoryFactory, { AUTH } from '@/repositories/RepositoryFactory'
+import { useRouter } from 'vue-router'
+const AuthRepository = RegistoryFactory[AUTH]
 
 export default defineComponent({
   components: {
     IonMenu,
-    IonHeader,
+    IonListHeader,
     IonContent,
     IonList,
     IonItem,
     IonLabel,
-    UserAvatar
+    UserAvatar,
+    IonButton
   },
   setup () {
     const userStore = useUserStore()
+    const router = useRouter()
+
+    const logout = async () => {
+      console.log('logout')
+      await AuthRepository.logout()
+      userStore.unset()
+      router.push('/')
+    }
 
     return {
-      user: userStore.get
+      user: userStore.get,
+      logout
     }
   }
 })
