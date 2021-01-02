3684faee5db16163af54165d925d21c623f64681
azukiazusa1 <cycleippei@gmail.com>
2021-01-02 17:59:28 +0900

    delete auth repository

--- src/repositories/RepositoryFactory.ts
+++ src/repositories/RepositoryFactory.ts
@@ -1,15 +1,11 @@
-import { AuthRepository, MockAuthRepository, AuthRepositoryInterface } from '@/repositories/auth'
 import { UserRepository, MockUserRepository, UserReositoryInterface } from '@/repositories/user'
 
-export const AUTH = Symbol('auth')
 export const USER = Symbol('user')
 
 export interface Repositories {
-  [AUTH]: AuthRepositoryInterface;
   [USER]: UserReositoryInterface;
 }
 
 export default {
-  [AUTH]: process.env.NODE_ENV === 'test' ? new MockAuthRepository() : new AuthRepository(),
   [USER]: process.env.NODE_ENV === 'test' ? new MockUserRepository() : new UserRepository()
 } as Repositories
