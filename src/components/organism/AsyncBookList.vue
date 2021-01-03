9698e4e69ca2553a0e71ed730ed25fbb32a24ab1
azukiazusa1 <cycleippei@gmail.com>
2021-01-03 17:38:56 +0900

    connect firebase functions

---
+++ src/components/organism/AsyncBookList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ res.data}}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'firebase/functions'
+import firebase from '@/plugins/firebase'
+
+export default defineComponent({
+  async setup () {
+    const functions = firebase.functions()
+    functions.useFunctionsEmulator('http://localhost:5001')
+    const func = functions.httpsCallable('books')
+    const res = await func({})
+    console.log(res)
+    console.log(functions)
+
+    return {
+      res
+    }
+  }
+})
+</script>
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,18 +1,22 @@
 <template>
   <ion-page>
-      <button @click="logout" >ログアウト</button>
+    <Suspense>
+      <async-book-list />
+    </Suspense>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage } from '@ionic/vue'
+import AsyncBookList from '@/components/organism/AsyncBookList.vue'
 import { useTitle } from 'vue-composable'
 import { APP_TITLE } from '@/constant'
 
 export default {
   name: 'Tab2',
   components: {
-    IonPage
+    IonPage,
+    AsyncBookList
   },
   setup () {
     const title = useTitle()
