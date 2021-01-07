8414aa15dd9a9405ba0410445dd2ed9a3d71ca66
azukiazusa1 <cycleippei@gmail.com>
2021-01-07 22:57:42 +0900

    loading component

---
+++ src/components/atoms/AppLoading.vue
@@ -0,0 +1,12 @@
+<template>
+   <ion-spinner name="circles"></ion-spinner>
+</template>
+
+<script lang="ts">
+import { IonSpinner } from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { IonSpinner }
+})
+</script>
--- src/views/SearchBook.vue
+++ src/views/SearchBook.vue
@@ -3,7 +3,14 @@
     <ion-content>
     <ion-searchbar animated placeholder="題名・著者・ISBN" v-model="q" />
     <Suspense v-if="q">
-      <async-book-list :q="q" />
+      <template #default>
+        <async-book-list :q="q" />
+      </template>
+      <template #fallback>
+        <div class="ion-text-center ion-padding-vertical">
+        <app-loading />
+        </div>
+      </template>
     </Suspense>
     </ion-content>
   </ion-page>
@@ -12,6 +19,7 @@
 <script lang="ts">
 import { IonPage, IonContent, IonSearchbar } from '@ionic/vue'
 import AsyncBookList from '@/components/organism/AsyncBookList.vue'
+import AppLoading from '@/components/atoms/AppLoading.vue'
 import { useTitle } from 'vue-composable'
 import { APP_TITLE } from '@/constant'
 import { defineComponent, ref } from 'vue'
@@ -21,7 +29,8 @@ export default defineComponent({
     IonPage,
     IonContent,
     IonSearchbar,
-    AsyncBookList
+    AsyncBookList,
+    AppLoading
   },
   setup () {
     const title = useTitle()
