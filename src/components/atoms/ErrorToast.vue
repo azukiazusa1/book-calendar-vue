0a788934cae79483a707c62aaa0ae496c78bb62d
azukiazusa1 <cycleippei@gmail.com>
2021-01-09 00:17:59 +0900

    error handling

---
+++ src/components/atoms/ErrorToast.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-toast
+    :is-open="isOpen"
+    :message="message"
+    color="danger"
+    position="middle"
+    :duration="2000"
+  >
+  </ion-toast>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonToast } from '@ionic/vue'
+export default defineComponent({
+  components: {
+    IonToast
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    message: {
+      type: String,
+      required: false,
+      default: '予期せぬエラーが発生しました。'
+    }
+  }
+})
+</script>
--- src/views/SearchBook.vue
+++ src/views/SearchBook.vue
@@ -5,6 +5,7 @@
         v-model:q="q"
         v-model:orderBy="orderBy"
       />
+      <error-toast :is-open="err" />
       <Suspense v-if="q">
         <template #default>
           <async-book-list :q="q" :orderBy="orderBy" />
@@ -21,10 +22,11 @@
 import { IonPage, IonContent } from '@ionic/vue'
 import AsyncBookList from '@/components/organism/AsyncBookList.vue'
 import SearchArea from '@/components/molecules/SearchArea.vue'
+import ErrorToast from '@/components/atoms/ErrorToast.vue'
 import SkeltonList from '@/components/molecules/SkeltonList.vue'
 import { useTitle } from 'vue-composable'
 import { APP_TITLE } from '@/constant'
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onErrorCaptured } from 'vue'
 import { q, orderBy } from '@/composables/use-searchBooks'
 
 export default defineComponent({
@@ -32,6 +34,7 @@ export default defineComponent({
     IonPage,
     IonContent,
     SearchArea,
+    ErrorToast,
     AsyncBookList,
     SkeltonList
   },
@@ -39,9 +42,17 @@ export default defineComponent({
     const title = useTitle()
     title.value = `本を探す | ${APP_TITLE}`
 
+    const err = ref<unknown>(null)
+
+    onErrorCaptured(e => {
+      err.value = e
+      return true
+    })
+
     return {
       q,
-      orderBy
+      orderBy,
+      err
     }
   }
 })
