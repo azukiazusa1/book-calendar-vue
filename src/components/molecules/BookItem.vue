6d87228199011993c070c8c7e91dfa39c4629bef
azukiazusa1 <cycleippei@gmail.com>
2021-01-11 23:06:31 +0900

    status buttons

--- src/components/molecules/BookItem.vue
+++ src/components/molecules/BookItem.vue
@@ -10,12 +10,18 @@
       <h2>{{ book.title }}</h2>
       <h3 v-if="book.auhtors">{{ book.auhtors[0] }}</h3>
       <p>{{ book.description }}</p>
+      <ion-badge class="ion-margin-top">読書中!</ion-badge>
+      <div class="ion-margin-top">
+        <ion-button class="button ion-margin-end" size="small" color="secondary">読書開始</ion-button>
+        <ion-button class="button ion-margin-end" size="small" color="tertiary">読了</ion-button>
+        <ion-button class="button ion-margin-end" size="small" color="success">ストック</ion-button>
+      </div>
     </ion-label>
   </ion-item>
 </template>
 
 <script lang="ts">
-import { IonItem, IonImg, IonLabel } from '@ionic/vue'
+import { IonItem, IonImg, IonLabel, IonBadge, IonButton } from '@ionic/vue'
 import { computed, defineComponent, PropType } from 'vue'
 import { BookItem } from '@/repositories/book'
 
@@ -23,7 +29,9 @@ export default defineComponent({
   components: {
     IonItem,
     IonImg,
-    IonLabel
+    IonLabel,
+    IonBadge,
+    IonButton
   },
   props: {
     book: {
@@ -46,6 +54,9 @@ export default defineComponent({
 <style scoped>
 .tuhmbnail {
   width: 80px;
-  height: 80%;
+}
+
+.button {
+  width: 60px;
 }
 </style>
