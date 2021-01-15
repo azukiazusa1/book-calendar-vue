ca286f9d624573e8c63b3a87f4739327928e52a0
azukiazusa1 <cycleippei@gmail.com>
2021-01-15 20:54:32 +0900

    modal header

---
+++ src/components/molecules/ModalHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>
+        <slot />
+      </ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="$emit('onDismiss')">キャンセル</ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonHeader, IonToolbar, IonTitle, IonButtons, IonButton } from '@ionic/vue'
+export default defineComponent({
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonButton
+  },
+  emits: {
+    onDismiss: () => true
+  }
+})
+</script>
