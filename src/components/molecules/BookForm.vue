c5fcfc413fe964e0dde1041c7c19789b56d28f6b
azukiazusa1 <cycleippei@gmail.com>
2021-01-15 20:58:19 +0900

    bookform

---
+++ src/components/molecules/BookForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-list>
+    <ion-list-header>
+      <ion-label>{{ title }}</ion-label>
+    </ion-list-header>
+    <ion-item>
+      <ion-label>読み始めた日</ion-label>
+      <ion-datetime display-format="YYYY/MM/DD" value="2012-12-15T13:47:20.789"></ion-datetime>
+    </ion-item>
+    <ion-item>
+      <ion-label>読み終わった日</ion-label>
+      <ion-datetime display-format="YYYY/MM/DD" value="2012-12-15T13:47:20.789"></ion-datetime>
+    </ion-item>
+    <ion-item>
+      <ion-label position="floating">感想</ion-label>
+      <ion-textarea rows="6" cols="20" placeholder="本を読み終えた感想、メモなどを入力しましょう！"></ion-textarea>
+    </ion-item>
+    <ion-button class="ion-margin-top" expand="block">登録</ion-button>
+  </ion-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonButton, IonList, IonListHeader, IonItem, IonLabel, IonDatetime, IonTextarea } from '@ionic/vue'
+
+export default defineComponent({
+  components: {
+    IonButton,
+    IonListHeader,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonDatetime,
+    IonTextarea
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
--- src/components/molecules/EditModal.vue
+++ src/components/molecules/EditModal.vue
@@ -1,52 +1,22 @@
 <template>
-  <ion-header>
-    <ion-toolbar>
-      <ion-title>読んだ本として登録</ion-title>
-      <ion-buttons slot="end">
-        <ion-button @click="$emit('onDismiss')">キャンセル</ion-button>
-      </ion-buttons>
-    </ion-toolbar>
-  </ion-header>
+  <modal-header @click="$emit('onDismiss')">読んだ本として登録<modal-header>
   <ion-content fullscreen>
-    <ion-list>
-      <ion-list-header>
-    <ion-label>{{ book.title }}</ion-label>
-  </ion-list-header>
-      <ion-item>
-        <ion-label>読み始めた日</ion-label>
-        <ion-datetime display-format="YYYY/MM/DD" value="2012-12-15T13:47:20.789"></ion-datetime>
-      </ion-item>
-      <ion-item>
-        <ion-label>読み終わった日</ion-label>
-        <ion-datetime display-format="YYYY/MM/DD" value="2012-12-15T13:47:20.789"></ion-datetime>
-      </ion-item>
-      <ion-item>
-        <ion-label position="floating">感想</ion-label>
-        <ion-textarea rows="6" cols="20" placeholder="本を読み終えた感想、メモなどを入力しましょう！"></ion-textarea>
-      </ion-item>
-      <ion-button class="ion-margin-top" expand="block">登録</ion-button>
-    </ion-list>
+    <book-form :title="book.title" />
   </ion-content>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import { IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonContent, IonList, IonListHeader, IonItem, IonLabel, IonDatetime, IonTextarea } from '@ionic/vue'
+import { IonContent } from '@ionic/vue'
 import { BookItem } from '@/repositories/book'
+import ModalHeader from '@/components/molecules/ModalHeader.vue'
+import BookForm from '@/components/molecules/BookForm.vue'
+
 export default defineComponent({
   components: {
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonButtons,
-    IonButton,
     IonContent,
-    IonListHeader,
-    IonList,
-    IonItem,
-    IonLabel,
-    IonDatetime,
-    IonTextarea
+    ModalHeader,
+    BookForm
   },
   props: {
     book: {
