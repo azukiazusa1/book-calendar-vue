184fb6929ea002b38ae195271799d02871db69b9
azukiazusa1 <cycleippei@gmail.com>
2021-01-16 16:54:06 +0900

    input date

--- src/components/molecules/BookForm.vue
+++ src/components/molecules/BookForm.vue
@@ -5,23 +5,44 @@
     </ion-list-header>
     <ion-item>
       <ion-label>読み始めた日</ion-label>
-      <ion-datetime display-format="YYYY/MM/DD" value="2012-12-15T13:47:20.789"></ion-datetime>
+      <ion-datetime display-format="YYYY/MM/DD"
+        v-model="data.startDate"
+      />
     </ion-item>
     <ion-item>
       <ion-label>読み終わった日</ion-label>
-      <ion-datetime display-format="YYYY/MM/DD" value="2012-12-15T13:47:20.789"></ion-datetime>
+      <ion-datetime display-format="YYYY/MM/DD"
+        v-model="data.endDate"
+      />
     </ion-item>
     <ion-item>
       <ion-label position="floating">感想</ion-label>
-      <ion-textarea rows="6" cols="20" placeholder="本を読み終えた感想、メモなどを入力しましょう！"></ion-textarea>
+      <ion-textarea
+        v-model="data.comment"
+        rows="6"
+        cols="20"
+        placeholder="本を読み終えた感想、メモなどを入力しましょう！" />
     </ion-item>
-    <ion-button class="ion-margin-top" expand="block">登録</ion-button>
+    <ion-button
+      class="ion-margin-top"
+      expand="block"
+      @click="onSubmit"
+    >
+      登録
+    </ion-button>
   </ion-list>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import { IonButton, IonList, IonListHeader, IonItem, IonLabel, IonDatetime, IonTextarea } from '@ionic/vue'
+import { useIntlDateTimeFormat } from 'vue-composable'
+
+type Data = {
+  startDate: string;
+  endDate: string;
+  comment: string;
+}
 
 export default defineComponent({
   components: {
@@ -37,6 +58,39 @@ export default defineComponent({
     title: {
       type: String,
       required: true
+    },
+    startDate: {
+      type: [Date, String],
+      required: false,
+      default: new Date()
+    },
+    endDate: {
+      type: [Date, String],
+      required: false,
+      default: new Date()
+    },
+    comment: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  setup (props) {
+    const { formatString } = useIntlDateTimeFormat('ja')
+
+    const data = reactive<Data>({
+      startDate: formatString(props.startDate),
+      endDate: formatString(props.endDate),
+      comment: props.comment
+    })
+
+    const onSubmit = () => {
+      console.log(data)
+    }
+
+    return {
+      data,
+      onSubmit
     }
   }
 })
