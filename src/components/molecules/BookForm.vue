bc2e183cf7d89e4d6d00b37b92a1801940b098a7
azukiazusa1 <cycleippei@gmail.com>
2021-01-16 21:21:42 +0900

    use validation

--- src/components/molecules/BookForm.vue
+++ src/components/molecules/BookForm.vue
@@ -6,26 +6,38 @@
     <ion-item>
       <ion-label>読み始めた日</ion-label>
       <ion-datetime display-format="YYYY/MM/DD"
-        v-model="data.startDate"
+        v-model="form.startDate.$value"
       />
     </ion-item>
     <ion-item>
       <ion-label>読み終わった日</ion-label>
       <ion-datetime display-format="YYYY/MM/DD"
-        v-model="data.endDate"
+        v-model="form.endDate.$value"
       />
     </ion-item>
     <ion-item>
       <ion-label position="floating">感想</ion-label>
       <ion-textarea
-        v-model="data.comment"
+        v-model="form.comment.$value"
         rows="6"
         cols="20"
         placeholder="本を読み終えた感想、メモなどを入力しましょう！" />
     </ion-item>
+      <div class="ion-margin-top">
+        <ion-text v-if="form.startDate.$anyInvalid" color="danger">
+          {{ form.startDate.$message }}
+        </ion-text>
+        <ion-text v-if="form.endDate.required.$invalid" color="danger">
+          {{ form.endDate.required.$message }}
+        </ion-text>
+        <ion-text v-if="form.endDate.compare.$invalid" color="danger">
+          {{ form.endDate.compare.$message }}
+        </ion-text>
+      </div>
     <ion-button
       class="ion-margin-top"
       expand="block"
+      :disabled="form.$anyInvalid"
       @click="onSubmit"
     >
       登録
@@ -34,9 +46,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
-import { IonButton, IonList, IonListHeader, IonItem, IonLabel, IonDatetime, IonTextarea } from '@ionic/vue'
-import { useIntlDateTimeFormat } from 'vue-composable'
+import { defineComponent } from 'vue'
+import { IonButton, IonList, IonListHeader, IonItem, IonLabel, IonDatetime, IonTextarea, IonText } from '@ionic/vue'
+import { useIntlDateTimeFormat, useValidation } from 'vue-composable'
 import { BookPayload } from '@/repositories/book'
 
 export default defineComponent({
@@ -47,7 +59,8 @@ export default defineComponent({
     IonItem,
     IonLabel,
     IonDatetime,
-    IonTextarea
+    IonTextarea,
+    IonText
   },
   props: {
     title: {
@@ -78,22 +91,43 @@ export default defineComponent({
   setup (props, { emit }) {
     const { formatString } = useIntlDateTimeFormat('ja')
 
-    const data = reactive<BookPayload>({
-      startDate: formatString(props.startDate),
-      endDate: formatString(props.endDate),
-      comment: props.comment
+    const form = useValidation({
+      startDate: {
+        $value: formatString(props.startDate),
+        required: (value: string) => !!value,
+        $message: '読み始めた日を入力してください。'
+      },
+      endDate: {
+        $value: formatString(props.endDate),
+        required: {
+          $validator: (value: string) => !!value,
+          $message: '読み終わった日を入力してください。'
+        },
+        compare: {
+          $validator: (value: string, context: any) => {
+            const startDate = new Date(context.startDate.$value)
+            const endDate = new Date(value)
+            return startDate.getTime() <= endDate.getTime()
+          },
+          $message: '読み終わった日は読み始めたよりも後の日付を入力してください。'
+        }
+      },
+      comment: {
+        $value: props.comment
+      }
     })
 
     const onSubmit = () => {
+      if (form.$anyInvalid) return
       emit('onSubmit', {
-        startDate: new Date(data.startDate),
-        endDate: new Date(data.endDate),
-        comment: data.comment
+        startDate: new Date(form.startDate.$value),
+        endDate: new Date(form.endDate.$value),
+        comment: form.comment.$value
       })
     }
 
     return {
-      data,
+      form,
       onSubmit
     }
   }
