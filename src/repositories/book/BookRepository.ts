220a3d8b91b7525987425e6c840c3b59d44fd96b
azukiazusa1 <cycleippei@gmail.com>
2021-01-16 21:38:57 +0900

    本を登録

--- src/repositories/book/BookRepository.ts
+++ src/repositories/book/BookRepository.ts
@@ -1,10 +1,22 @@
 import 'firebase/functions'
 import firebase from '@/plugins/firebase'
+import db from '@/plugins/db'
+import { useAuth } from '@/composables/use-auth'
 import { Params, BookRepositoryInterface, Result, BookItem } from './types'
 
 const functions = firebase.functions()
 functions.useEmulator('localhost', 5001)
 const func = functions.httpsCallable('books')
+
+const { auth } = useAuth()
+const useBookRef = async () => {
+  const user = await auth()
+  if (!user) {
+    throw new Error('ログインが必要です。')
+  }
+
+  return db.collection('users').doc(user.uid).collection('books')
+}
 
 export class BookRepository implements BookRepositoryInterface {
   async find (params: Params) {
@@ -12,7 +24,13 @@ export class BookRepository implements BookRepositoryInterface {
     return result.data.data as Result
   }
 
-  regist (book: BookItem) {
-    return Promise.resolve(book)
+  async regist (book: BookItem) {
+    const bookRef = await useBookRef()
+    const doc = await bookRef.add(book)
+    console.log(doc)
+    return {
+      docId: doc.id,
+      ...book
+    }
   }
 }
