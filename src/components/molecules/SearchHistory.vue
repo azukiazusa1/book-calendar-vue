2a06572f41ab2314ac57afb9c54334694f146594
azukiazusa1 <cycleippei@gmail.com>
2021-01-20 20:09:26 +0900

    searchHistoryWord

---
+++ src/components/molecules/SearchHistory.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-list>
+    <search-history-word v-for="(word, i) in searchWords" :key="i" :word="word" />
+  </ion-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonList } from '@ionic/vue'
+import SearchHistoryWord from '@/components/atoms/SearchHistoryWord.vue'
+
+export default defineComponent({
+  components: {
+    IonList,
+    SearchHistoryWord
+  },
+  porps: {
+    searchWords: {
+      type: Array,
+      required: true
+    }
+  }
+})
+</script>
