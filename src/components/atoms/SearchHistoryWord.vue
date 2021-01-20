25dedb6e686b750500d4d7689891f9963d55ef0a
azukiazusa1 <cycleippei@gmail.com>
2021-01-20 20:26:49 +0900

    search-word.vue

---
+++ src/components/atoms/SearchHistoryWord.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-item button :detail-icon="arrowUpOutline">
+    <ion-label>
+      {{ word }}
+    </ion-label>
+  </ion-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { arrowUpOutline } from 'ionicons/icons'
+
+export default defineComponent({
+  props: {
+    word: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    return {
+      arrowUpOutline
+    }
+  }
+})
+</script>
