39a2b7748b415055fe9b70ab8a553f903f82456e
azukiazusa1 <cycleippei@gmail.com>
2021-01-22 21:09:54 +0900

    すでに存在しているワードが追加された場合先頭に移動します

--- src/composables/use-storage.ts
+++ src/composables/use-storage.ts
@@ -1,9 +1,14 @@
 import { ref, readonly } from 'vue'
 
+/** ローカルストレージのキー */
 const KEY = 'search-word'
+/** ローカルストレージの値をリアクティブに保持します。 */
 const storage = ref<string[]>([])
 
 export const useStorage = () => {
+  /**
+   * ローカルストレージから、配列の形式で値を取得します。
+   */
   const get = () => {
     try {
       const value = localStorage.getItem(KEY) ?? '[]'
@@ -13,18 +18,34 @@ export const useStorage = () => {
     }
   }
 
-  const set = (value: string) => {
-    const newValues = [...get(), value]
-    localStorage.setItem(KEY, JSON.stringify(newValues))
-    storage.value = newValues
-  }
-
+  /**
+   * 引数に渡したワードを削除します。
+   * @param value
+   */
   const remove = (value: string) => {
     const newValues = get().filter(v => v !== value)
     localStorage.setItem(KEY, JSON.stringify(newValues))
     storage.value = newValues
   }
 
+  /**
+   * 新たにワードをセットします。
+   * すでに存在している場合、先頭に移動させます。
+   * @param value
+   */
+  const set = (value: string) => {
+    const values = get()
+    if (values.includes(value)) {
+      values.filter(v => v !== value)
+    }
+    values.push(value)
+    localStorage.setItem(KEY, JSON.stringify(values))
+    storage.value = values
+  }
+
+  /**
+   * すべてのワードを削除します。
+   */
   const clear = () => {
     localStorage.setItem(KEY, '[]')
     storage.value = []
