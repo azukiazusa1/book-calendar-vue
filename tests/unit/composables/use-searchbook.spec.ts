22928a71b3a365c05765090fac23da5b9ca440ee
azukiazusa1 <cycleippei@gmail.com>
2021-01-24 22:40:53 +0900

    ｑ

--- tests/unit/composables/use-searchbook.spec.ts
+++ tests/unit/composables/use-searchbook.spec.ts
@@ -1,17 +1,33 @@
 import { useSearchBooks } from '@/composables/use-searchBooks'
+import { start } from '@popperjs/core'
 import flushPromises from 'flush-promises'
-const { startIndex, result, q, orderBy } = useSearchBooks()
+const { startIndex, result, loading, q, orderBy } = useSearchBooks()
 
 jest.mock('lodash.debounce', () => (fn: any) => {
   return fn
 })
 
+const initialize = () => {
+  startIndex.value = 1
+  result.value = null
+  loading.value = true
+}
+
 describe('@/composables/use-search-book', () => {
+  beforeEach(() => {
+    initialize()
+  })
   describe('watch params', () => {
     test('qが変更されるとrepositoryからデータを取得します。', async () => {
       q.value = 'test'
       await flushPromises()
       expect(result.value).not.toBeNull()
+    })
+
+    test('qが空の場合にはデータを取得しない', async () => {
+      q.value = ' '
+      await flushPromises()
+      expect(result.value).toBeNull()
     })
   })
 })
