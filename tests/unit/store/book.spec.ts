d811d4e7b1f255141de5671fd746b6c5a7434f65
azukiazusa1 <cycleippei@gmail.com>
2021-01-26 23:11:59 +0900

    setBooks

--- tests/unit/store/book.spec.ts
+++ tests/unit/store/book.spec.ts
@@ -1,6 +1,7 @@
 import { createDummyBook, createDummyBooks } from '@/composables/utils'
 import { useBookStore } from '@/store/book'
-const { state, clearBooks, addBooks } = useBookStore()
+import { start } from '@popperjs/core'
+const { state, clearBooks, addBooks, setBooks } = useBookStore()
 
 describe('@/store/book', () => {
   beforeEach(() => {
@@ -23,6 +24,16 @@ describe('@/store/book', () => {
         addBooks(createDummyBook())
         expect(state.books.length).toBe(4)
       })
+    })
+  })
+
+  describe('setBooks', () => {
+    test('stateのbooksを置き換えます', () => {
+      setBooks(createDummyBooks(3))
+      expect(state.books.length).toBe(3)
+
+      setBooks(createDummyBooks(4))
+      expect(state.books.length).toBe(4)
     })
   })
 })
