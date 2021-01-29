0c2df527feb3004d8338234b9ad337149971d4c7
azukiazusa1 <cycleippei@gmail.com>
2021-01-29 23:18:18 +0900

    BookDetails Component

---
+++ src/views/BookDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  {{ id }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  setup () {
+    const route = useRoute()
+
+    return {
+      id: route.params.id
+    }
+  }
+})
+</script>
