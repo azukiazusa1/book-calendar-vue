eb7e4c2856562021eabb391df8b8609a3adb74c8
azukiazusa1 <cycleippei@gmail.com>
2021-04-08 23:15:12 +0900

    fix unit test

--- src/repositories/book/MockBookRepository.ts
+++ src/repositories/book/MockBookRepository.ts
@@ -1,11 +1,8 @@
 import { BookItem, BookRepositoryInterface, Params } from './types'
-import { dummyLoading } from '@/composables/utils'
 import items from './dummyBooks'
 
 export class MockBookRepository implements BookRepositoryInterface {
   async find (_: Params) {
-    await dummyLoading()
-    console.log(items)
     return Promise.resolve({
       kind: 'books#volumes',
       totalItems: items.length,
@@ -14,7 +11,6 @@ export class MockBookRepository implements BookRepositoryInterface {
   }
 
   async findById (id: string) {
-    await dummyLoading()
     const book = items.find(book => book.id === id)
     if (!book) {
       throw new Error('book not found')
