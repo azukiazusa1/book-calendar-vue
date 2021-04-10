50c4d79b82caade597a2eab6f781fd45bdf8782c
azukiazusa1 <cycleippei@gmail.com>
2021-04-10 22:50:22 +0900

    small size thumbnail test

--- tests/unit/composables/use-bookUtils.spec.ts
+++ tests/unit/composables/use-bookUtils.spec.ts
@@ -1,6 +1,6 @@
 import { useBookUtils } from '@/composables/use-bookUtils'
 import { createDummyBook } from '@/composables/utils'
-const { thumbnail, striptTagsDescription } = useBookUtils()
+const { thumbnail } = useBookUtils()
 
 describe('@/composables/use-bookUtils', () => {
   const book = createDummyBook()
@@ -9,6 +9,10 @@ describe('@/composables/use-bookUtils', () => {
     test('size mediumを指定したとき、通常のサムネイルを返す', () => {
       const result = thumbnail(book, 'medium')
       expect(result.value).toEqual('https://placehold.jp/350x240.png')
+    })
+    test('size smallを指定したとき、小さめのサムネイルを返す', () => {
+      const result = thumbnail(book, 'small')
+      expect(result.value).toEqual('https://placehold.jp/150x150.png')
     })
   })
 })
