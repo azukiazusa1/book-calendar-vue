dcd6c15a7cbb352cfaba3a96ee6cb9cf5b603ed5
azukiazusa1 <cycleippei@gmail.com>
2021-04-12 23:00:59 +0900

    f

--- tests/unit/composables/use-bookUtils.spec.ts
+++ tests/unit/composables/use-bookUtils.spec.ts
@@ -3,16 +3,31 @@ import { createDummyBook } from '@/composables/utils'
 const { thumbnail } = useBookUtils()
 
 describe('@/composables/use-bookUtils', () => {
-  const book = createDummyBook()
-
   describe('thumbnail', () => {
     test('size mediumを指定したとき、通常のサムネイルを返す', () => {
+      const book = createDummyBook()
       const result = thumbnail(book, 'medium')
       expect(result.value).toEqual('https://placehold.jp/350x240.png')
     })
+
     test('size smallを指定したとき、小さめのサムネイルを返す', () => {
+      const book = createDummyBook()
       const result = thumbnail(book, 'small')
       expect(result.value).toEqual('https://placehold.jp/150x150.png')
+    })
+
+    test('size mediumを指定した時、通常のサムネイルが存在しないときはno imageを返す', () => {
+      const book = createDummyBook()
+      book.imageLinks = undefined
+      const result = thumbnail(book, 'medium')
+      expect(result.value).toEqual('/assets/icon/no-image.png')
+    })
+
+    test('size smallを指定した時、小さめのサムネイルが存在しないときはno imageを返す', () => {
+      const book = createDummyBook()
+      book.imageLinks = undefined
+      const result = thumbnail(book, 'small')
+      expect(result.value).toEqual('/assets/icon/no-image.png')
     })
   })
 })
